6e1ccdf361b319d06236f5f37a59bc023595edac
Mustafa <mustafamcm9@gmail.com>
2023-02-09 17:16:13 +0300

    header and add book button created

---
+++ src/script.js
@@ -0,0 +1,19 @@
+const header = document.querySelector("header");
+
+// Try to make a modal book information menu with this api
+
+// const options = {
+//   method: "GET",
+//   headers: {
+//     "X-RapidAPI-Key": "fcb893824cmsh1a10f5c1a7e2fe5p104ffbjsnaf212e4f6007",
+//     "X-RapidAPI-Host": "hapi-books.p.rapidapi.com",
+//   },
+// };
+
+// fetch(
+//   "https://hapi-books.p.rapidapi.com/search/a+song+of+ice+and+fire",
+//   options
+// )
+//   .then((response) => response.json())
+//   .then((response) => console.log(response))
+//   .catch((err) => console.error(err));
