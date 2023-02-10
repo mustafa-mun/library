7c36922150aff1d183ed9b201d6571168c3af4bf
Mustafa <mustafamcm9@gmail.com>
2023-02-10 17:58:58 +0300

    local storage is working but definietly needs to be refactored

--- src/script.js
+++ src/script.js
@@ -7,6 +7,8 @@ const addBookMenuBtn = document.getElementById("add-book-menu-btn");
 const modal = document.getElementById("add-book-modal");
 const booksSection = document.getElementById("books-section");
 
+// Change books read status in respect to local storage items read status
+
 class Book {
   constructor(title, author, pages, read) {
     this.title = title;
@@ -17,10 +19,16 @@ class Book {
 }
 
 const myLibrary = [];
+const newArr = JSON.parse(localStorage.getItem("book"));
+
+if (newArr) {
+  createStorageElements(newArr);
+}
 
 function toggleModal() {
   modal.classList.toggle("visible");
 }
+
 
 function addBooksToLibrary() {
   const title = bookTitleInput.value;
@@ -29,9 +37,17 @@ function addBooksToLibrary() {
   const readStatusCheck = readStatusInput.checked;
   const book = new Book(title, author, pages, readStatusCheck);
   myLibrary.push(book);
+  localStorage.setItem("book", JSON.stringify(myLibrary));
+  if (newArr) {
+    newArr.push(book);
+    localStorage.setItem("book", JSON.stringify(newArr));
+  }
 }
 
-function createElements() {
+
+function createElements(array) {
+  // Create book card
+
   const div = document.createElement("div");
   const iconsContainer = document.createElement("div");
   const infoIcon = document.createElement("img");
@@ -41,7 +57,7 @@ function createElements() {
   const bookAuthor = document.createElement("p");
   const bookPages = document.createElement("p");
   const statusBtn = document.createElement("button");
-  myLibrary.forEach((item) => {
+  array.forEach((item) => {
     iconsContainer.className = "close-i-container";
     infoIcon.setAttribute("src", "assets/letter-i.png");
     infoIcon.setAttribute("name", "info-icon");
@@ -62,8 +78,22 @@ function createElements() {
       statusBtn.style.backgroundColor = "green";
       statusBtn.textContent = "READ";
     }
-  });
 
+    statusBtn.addEventListener("click", () => {
+      if (item.read) {
+        item.read = false;
+      } else {
+        item.read = true;
+      }
+      if (!item.read) {
+        statusBtn.style.backgroundColor = "green";
+        statusBtn.textContent = "READ";
+      } else {
+        statusBtn.style.backgroundColor = "red";
+        statusBtn.textContent = "UNREAD";
+      }
+    });
+  });
   booksSection.appendChild(div);
   div.appendChild(iconsContainer);
   iconsContainer.appendChild(infoIcon);
@@ -73,28 +103,87 @@ function createElements() {
   bookInfo.appendChild(bookAuthor);
   bookInfo.appendChild(bookPages);
   div.appendChild(statusBtn);
-
-  statusBtn.addEventListener("click", () => {
-    if (statusBtn.style.backgroundColor === "red") {
-      statusBtn.style.backgroundColor = "green";
-      statusBtn.textContent = "READ";
-    } else {
-      statusBtn.style.backgroundColor = "red";
-      statusBtn.textContent = "UNREAD";
-    }
-  });
-
-  closeIcon.addEventListener("click", () => {
-    booksSection.removeChild(div);
-  });
+  array.forEach(item => {
+    closeIcon.addEventListener("click", () => {
+      const index = array.indexOf(item)
+      newArr.splice(index,1)
+      localStorage.setItem("book", JSON.stringify(newArr))
+      booksSection.removeChild(div);
+    });
+  })
 }
-
 form.addEventListener("submit", (event) => {
   event.preventDefault();
   addBooksToLibrary();
-  createElements();
+  createElements(myLibrary);
   toggleModal();
 });
+
+function createStorageElements(array) {
+  array.forEach((item) => {
+    const div = document.createElement("div");
+    const iconsContainer = document.createElement("div");
+    const infoIcon = document.createElement("img");
+    const closeIcon = document.createElement("img");
+    const bookInfo = document.createElement("div");
+    const bookTitle = document.createElement("p");
+    const bookAuthor = document.createElement("p");
+    const bookPages = document.createElement("p");
+    const statusBtn = document.createElement("button");
+    iconsContainer.className = "close-i-container";
+    infoIcon.setAttribute("src", "assets/letter-i.png");
+    infoIcon.setAttribute("name", "info-icon");
+    closeIcon.setAttribute("src", "assets/close.png");
+    closeIcon.setAttribute("name", "close-icon");
+    infoIcon.id = "i-icon";
+    closeIcon.id = "close-icon";
+    bookInfo.className = "book-information";
+    bookTitle.textContent = `TITLE:  ${item.title}`;
+    bookAuthor.textContent = `AUTHOR: ${item.author}`;
+    bookPages.textContent = `${item.pages} pages`;
+    statusBtn.className = "read-unread-btn";
+
+    statusBtn.addEventListener("click", () => {
+      if (!item.read) {
+        item.read = true;
+      } else {
+        item.read = false;
+      }
+      if (item.read) {
+        statusBtn.style.backgroundColor = "green";
+        statusBtn.textContent = "READ";
+      } else {
+        statusBtn.style.backgroundColor = "red";
+        statusBtn.textContent = "UNREAD";
+      }
+    });
+
+    if (!item.read) {
+      statusBtn.style.backgroundColor = "red";
+      statusBtn.textContent = "UNREAD";
+    } else {
+      statusBtn.style.backgroundColor = "green";
+      statusBtn.textContent = "READ";
+    }
+
+    booksSection.appendChild(div);
+    div.appendChild(iconsContainer);
+    iconsContainer.appendChild(infoIcon);
+    iconsContainer.appendChild(closeIcon);
+    div.appendChild(bookInfo);
+    bookInfo.appendChild(bookTitle);
+    bookInfo.appendChild(bookAuthor);
+    bookInfo.appendChild(bookPages);
+    div.appendChild(statusBtn);
+
+    closeIcon.addEventListener("click", () => {
+      const index = array.indexOf(item)
+      newArr.splice(index,1)
+      localStorage.setItem("book", JSON.stringify(newArr))
+      booksSection.removeChild(div);
+    });
+  });
+}
 
 addBookMenuBtn.addEventListener("click", () => {
   // Toggle book add menu
